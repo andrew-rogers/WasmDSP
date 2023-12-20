fde384bd68153d048e4b1c34570d6726da01cd44
Andrew Rogers <andrew.rogerstech@gmail.com>
2023-12-20 15:50:07 +0000

    Get output of fractional sampler example.

--- examples/fractional_resampler/fractional_resampler.cpp
+++ examples/fractional_resampler/fractional_resampler.cpp
@@ -1,10 +1,11 @@
 #include "FractionalResampler.h"
 #include "JsMediator.h"
+#include "emjs.h"
 
 #include <cstdint>
 #include <emscripten.h>
 
-WasmDSP::JsMediator mediator;
+WasmDSP::IMediator* mediator = &WasmDSP::g_mediator;
 
 EMSCRIPTEN_KEEPALIVE
 extern "C" void* allocFloat32(const size_t size)
@@ -15,19 +16,12 @@ extern "C" void* allocFloat32(const size_t size)
 EMSCRIPTEN_KEEPALIVE
 extern "C" FractionalResampler* FractionalResampler_new(const float rate, float* input, size_t input_len, float* output, size_t output_max)
 {
-	return new FractionalResampler(rate, &mediator, input, input_len, output, output_max);
+	return new FractionalResampler(rate, mediator, input, input_len, output, output_max);
 }
 
 EMSCRIPTEN_KEEPALIVE
 extern "C" void FractionalResampler_resample(FractionalResampler* fr)
 {
     fr->resample();
-}
-
-EMSCRIPTEN_KEEPALIVE
-extern "C" void test_notify(uint32_t i)
-{
-    WasmDSP::IMediator* ptr = &mediator;
-    ptr->notify((void*)10,i+3);
 }
 

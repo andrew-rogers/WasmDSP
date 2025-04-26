433aaf869ac66cf7637e7d88154d4fef68fd3560
Andrew Rogers <andrew.rogerstech@gmail.com>
2025-04-26 17:53:23 +0100

    UpFIRDown indexes.

---
+++ modules/signal/UpFIRDown.cpp
@@ -0,0 +1,56 @@
+/*
+ * WasmDSP - Polyphase resampling based on FIR filter.
+ * Copyright (C) 2025  Andrew Rogers
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "UpFIRDown.h"
+
+size_t UpFIRDown::processBlock( const float* x, float* y, size_t num_x )
+{
+    
+    // Copy samples into buffer.
+    for (size_t n = 0U; n < num_x; n++)
+    {
+        m_buffer[num_coeffs + m_cnt] = x[n];
+        m_cnt++;
+    }
+
+    // Calculate number of output samples.
+    size_t num_y = m_cnt / m_Q;
+    num_y *= m_P;
+
+    size_t hi_start = 0U;
+    size_t bi_start = num_coeffs;
+    for (int yi = 0; yi < num_y; yi++)
+    {
+        // Calculate the indexes for the first element of each MAC.
+        hi_start+=Q;
+        if (hi_start>=P)
+        {
+            hi_start -= P;
+            bi_start ++;
+        }
+    }
+
+    return num_y;
+}
---
+++ modules/signal/UpFIRDown.h
@@ -0,0 +1,44 @@
+/*
+ * WasmDSP - Polyphase resampling based on FIR filter.
+ * Copyright (C) 2025  Andrew Rogers
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef UPFIRDOWN_H
+#define UPFIRDOWN_H
+
+#include <cstdlib>
+
+class UpFIRDown
+{
+public:
+    UpFIRDown(size_t P, size_t Q, size_t max_block_size, const float * coeffs, size_t num_coeffs)
+    {
+    }
+    
+    size_t processBlock( const float* x, float* y, size_t num_x ); // Returns number of output samples.
+
+private:
+};
+
+#endif // UPFIRDOWN_H
+

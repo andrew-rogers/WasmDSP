fb34b6a4ce090133b8c16d780646b042a34ceb4e
Andrew Rogers <andrew.rogerstech@gmail.com>
2025-08-30 09:03:41 +0100

    Move digital filter stuff from butter to filter and generalise.

--- modules/signal/filter.mjs
+++ modules/signal/filter.mjs
@@ -28,19 +28,13 @@
 import {Ellip, R, K, F} from './ellip'
 
 export function ellip(N, rp, rs, fc, bt, options) {
-  // TODO: Check band
-  // TODO: Check options, {analogue: false, zpk: true} etc.
-  // TODO: Pre-warp
+  let opt = checkOptions(options);
 
   // Prototype
   let [z, p, k] = ellipap(N, rp, rs);
 
   // Frequency scale
-  [z, p, k] = frequencyScale(z, p, k, fc, bt);
-
-  // TODO: BZT
-
-  return [z, p, k];
+  return frequencyScale(z, p, k, fc, bt, opt);
 }
 
 export function ellipap(N, rp, rs) {
@@ -71,6 +65,22 @@ export function ellipap(N, rp, rs) {
   return [z, p, 1];
 }
 
+function bzt(z, p, k) {
+  function f(a) {
+    return complexDiv([2 + a[0], a[1]], [2 - a[0], -a[1]])
+  }
+  z = z.map(f);
+  p = p.map(f);
+  return [z, p, k];
+}
+
+function checkOptions(options) {
+  let opt = options || {};
+  if (!opt.hasOwnProperty('digital')) opt.digital = true;
+  if (!opt.hasOwnProperty('sos')) opt.sos = true;
+  return opt;
+}
+
 function complexDiv(num, den) {
   const a = num[0];
   const b = num[1];
@@ -87,7 +97,7 @@ function complexDiv(num, den) {
   return [scale*r, scale*i];
 }
 
-function frequencyScale(z, p, k, fc, bt) {
+function frequencyScale(z, p, k, fc, bt, opt) {
   bt = bt || 'L'; // Default to lowpass.
 
   // find first occurance of L,H,P or S
@@ -95,6 +105,8 @@ function frequencyScale(z, p, k, fc, bt) {
   bt = bt.toUpperCase().match(/[LHPS]/g);
   if (bt.length == 0) bt = 'L';
   else bt = bt[0];
+
+  if (opt.digital) fc = preWarp(fc); // TODO: handle two frequencies for bandpas / bandstop
 
   // Scale zeros and poles
   if (bt == 'H') { // TODO: 'P' and 'S'
@@ -107,5 +119,42 @@ function frequencyScale(z, p, k, fc, bt) {
     p = p.map((v) => [fc * v[0], fc * v[1]]);
   }
 
+  if (opt.digital) [z, p, k] = bzt(z, p, k);
+
+  if (opt.sos) return zpk2sos(z, p, k);
   return [z, p, k];
+}
+
+function preWarp(f) {
+  return 2 * Math.tan(Math.PI * f * 0.5);
+}
+
+export function zpk2sos(z,p,k) {
+
+  // Create unity SOS array.
+  let sos = [];
+  while ((sos.length * 2) < z.length) sos.push([1, 0, 0, 1, 0, 0]);
+  while ((sos.length * 2) < p.length) sos.push([1, 0, 0, 1, 0, 0]);
+
+  // Modify sections for zeros
+  let N = z.length;
+  let odd = N % 2;
+  let Npairs = (N - odd) / 2;
+  for (let n = 0; n < Npairs; n++) {
+    sos[n][1] = -2 * z[n][0];
+    sos[n][2] = (z[n][0] ** 2) + (z[n][1] ** 2);
+  }
+  if(odd) sos[Npairs][1] = -z[Npairs][0];
+
+  // Modify sections for poles
+  N = p.length;
+  odd = N % 2;
+  Npairs = (N - odd) / 2;
+  for (let n = 0; n < Npairs; n++) {
+    sos[n][4] = -2 * p[n][0];
+    sos[n][5] = (p[n][0] ** 2) + (p[n][1] ** 2);
+  }
+  if(odd) sos[Npairs][4] = -p[Npairs][0];
+
+  return sos;
 }
